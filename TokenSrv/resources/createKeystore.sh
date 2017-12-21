4dda23ffd2f8cd2915b6d225177288c7f5be650b
turkmenf <fturkmen@uva.nl>
2017-12-21 21:43:29 +0100

    fixes

--- TokenSrv/resources/createKeystore.sh
+++ TokenSrv/resources/createKeystore.sh
@@ -14,6 +14,6 @@ VALIDITY=365
 
 KEYSIZE=2048
 
-keytool -genkey -alias $ALIAS -dname "CN=DACI Token Service, OU=SNE Group, O=UvA, C=NL" -validity $VALIDITY -keypass $KEYPASS -keyalg "RSA" -keysize $KEYSIZE -keystore ./TokenSrv/tokenSrvFiles/$KEYSTORE -storepass $STOREPASS -storetype $STORETYPE
+keytool -genkey -alias $ALIAS -dname "CN=DACI Token Service, OU=SNE Group, O=UvA, C=NL" -validity $VALIDITY -keypass $KEYPASS -keyalg "RSA" -keysize $KEYSIZE -keystore tokenSrvFiles/$KEYSTORE -storepass $STOREPASS -storetype $STORETYPE
 
-keytool -exportcert -file ./TokenSrv/tokenSrvFiles/"$ALIAS.crt" -keystore ./TokenSrv/tokenSrvFiles/$KEYSTORE -storepass $STOREPASS -alias $ALIAS -rfc
+keytool -exportcert -file tokenSrvFiles/"$ALIAS.crt" -keystore tokenSrvFiles/$KEYSTORE -storepass $STOREPASS -alias $ALIAS -rfc
